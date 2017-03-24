736759a3b2b5032bed73dfa9cbc4bcd86d548677
Tony Arcieri <bascule@gmail.com>
2017-03-24 00:38:37 -0700

    More minor build cleanups

--- ci.sh
+++ ci.sh
@@ -12,13 +12,13 @@ js)
     ;;
 ruby)
     ruby -v
-    gem install bundler --version 1.14.5 --no-rdoc --no-ri
     bundle --version
     cd ruby
     bundle
     bundle exec rake
     ;;
 rust)
+    rustc --version
     cd rust
     cargo test
     ;;
