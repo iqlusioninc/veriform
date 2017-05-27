4bd2c1bca6691b9bb3ec125f802b9d8f39428029
Tony Arcieri <bascule@gmail.com>
2017-05-27 12:00:24 -0700

    js: Cleanups

--- js/src/decoder.ts
+++ js/src/decoder.ts
@@ -1,47 +1,50 @@
 import { Handler } from "./parser";
 
 export class Decoder implements Handler<object> {
-  private currentObject: any;
-  private stack: any[];
+  private stack: object[];
 
   constructor() {
-    this.currentObject = {};
-    this.stack = [];
+    this.stack = [{}];
   }
 
   // Add a uint64 to the current object
   uint64(id: number, value: number) {
-    this.currentObject[id] = value;
+    this.currentObject()[id] = value;
   }
 
   // Add binary data to the current object
   binary(id: number, value: Uint8Array) {
-    this.currentObject[id] = value;
+    this.currentObject()[id] = value;
   }
 
   // Push down the internal stack, constructing a new object
   beginNested() {
-    this.stack.push(this.currentObject);
-    this.currentObject = {};
+    this.stack.push({});
   }
 
   // End a nested object, setting it to the given ID on its parent
   endNested(id: number) {
+    let value = this.stack.pop();
+
     if (this.stack.length === 0) {
       throw new Error("not inside a nested message");
     }
 
-    let value = this.currentObject;
-    this.currentObject = this.stack.pop();
-    this.currentObject[id] = value;
+    this.currentObject()[id] = value;
   }
 
   // Finish decoding, returning the finished parent object
   finish(): any {
+    let result = this.stack.pop();
+
     if (this.stack.length !== 0) {
       throw new Error("objects remaining in stack");
     }
 
-    return this.currentObject;
+    return result;
+  }
+
+  private currentObject(): any {
+    return this.stack[this.stack.length - 1];
   }
 }
